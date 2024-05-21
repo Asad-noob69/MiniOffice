4306f0c5a39eacdc148e0871139955159e10c080
Asad <asadaliabbasi787@gmail.com>
2024-05-21 06:13:58 -0700

    first

---
+++ script.js
@@ -0,0 +1,40 @@
+const fontSelect = document.getElementById('fontSelect');
+const textToChange = document.getElementById('textChange');
+
+fontSelect.addEventListener('change', function(){
+        const selectedFont = fontSelect.value;
+        textToChange.style.fontFamily= selectedFont;
+
+});
+
+function toggleBold(){
+    document.execCommand('bold');
+}
+function toggleItalic(){
+    document.execCommand('italic');
+}
+
+
+
+function changeBgColor(){
+    const color = document.getElementById('bgColor').value;
+const editor = document.querySelector('editor');
+    editor.style.backgroundColor = bgColor;
+}
+
+
+    function alignText(alignment) {
+        switch(alignment) {
+        case 'left':
+            document.execCommand('justifyLeft');
+            break;
+            case 'right' :
+            document.execCommand('justifyRight');
+            break;
+            case 'center':
+                document.execCommand('justifyCenter');
+                break;
+                default:
+                    console.error('unknownAlignment' + alignment);
+    }
+    }
